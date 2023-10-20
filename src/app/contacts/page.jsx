3de150ba0076f9989fdf396d0b78f611af6710f7
Vincenzo De Pascale <119123124+VincenzoDePascale@users.noreply.github.com>
2023-10-20 12:56:32 +0200

    fix contactMe

--- src/app/contacts/page.jsx
+++ src/app/contacts/page.jsx
@@ -1,9 +1,58 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import emailjs from "emailjs-com";
+import { useRef } from "react";
 import "./page.css";
 import ContactForm from "./ContactForm";
 
 export default function Contacts() {
+  const form = useRef();
+  const [formData, setFormData] = useState({
+    user_name: "",
+    user_email: "",
+    message: "",
+  });
+  const [alertMessage, setAlertMessage] = useState("");
+
+  useEffect(() => {
+    // chiave pubblica di Email.js
+    emailjs.init("1YGv38iIJPTJEEKnV");
+  }, []);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [name]: value,
+      };
+    });
+  };
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm("service_8d1r9ot", "template_wuss68q", form.current).then(
+      (result) => {
+        console.log(result.text);
+        setFormData({
+          user_name: "",
+          user_email: "",
+          message: "",
+        });
+        setAlertMessage(
+          "Grazie per avermi inviato un'email, ti risponderò al più presto."
+        );
+      },
+      (error) => {
+        setAlertMessage(
+          "Mi dispiace qualcosa è andato storto, riprova tra qualche minuto o contattami su Linkedin."
+        );
+        console.log(error.text);
+      }
+    );
+  };
+
   return (
     <>
       <div className="text-white box md:mt-[100px]">
@@ -91,11 +140,64 @@ export default function Contacts() {
               </a>
             </div>
 
-            <div className="flex longness">
-              {/* mail box */}
-              <div className="flex-col items-center basis-full bordBox p-4">
-                <ContactForm />
-              </div>
+            {/* mail box */}
+
+            <div className="flex-col longness items-center basis-full flex-grow-0 bordBox p-4">
+              {alertMessage && <div className="alert">{alertMessage}</div>}
+              <h2 className="text-xl font-semibold m-2 text-center flex justify-center">
+                Contact me
+              </h2>
+              <form
+                ref={form}
+                onSubmit={sendEmail}
+                className="md:max-w-[800px] md:m-auto md:py-5"
+              >
+                <div className="mx-5 flex flex-col justify-center">
+                  <div className="labelBox flex flex-col mb-5">
+                    <label htmlFor="name">Name:</label>
+                    <input
+                      className="bgInnerText text-black rounded"
+                      type="text"
+                      id="name"
+                      name="user_name"
+                      value={formData.user_name}
+                      onChange={handleInputChange}
+                      required
+                    />
+                  </div>
+                  <div className="labelBox flex flex-col mb-5">
+                    <label htmlFor="email">Email:</label>
+                    <input
+                      className="bgInnerText text-black rounded"
+                      type="email"
+                      id="email"
+                      name="user_email"
+                      value={formData.user_email}
+                      onChange={handleInputChange}
+                      required
+                    />
+                  </div>
+                  <div className="labelBox flex flex-col mb-5">
+                    <label htmlFor="message">Message:</label>
+                    <textarea
+                      className="bgInnerText text-black rounded"
+                      id="message"
+                      name="message"
+                      value={formData.message}
+                      onChange={handleInputChange}
+                      required
+                    />
+                  </div>
+                  <div className="flex flex-row-reverse pt-5">
+                    <button
+                      className="button col w-[85px] px-2 py-1 bordBox"
+                      type="submit"
+                    >
+                      Send
+                    </button>
+                  </div>
+                </div>
+              </form>
             </div>
           </div>
         </div>
