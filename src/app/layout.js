49401932f1e702ac11c82fad371bc0e060f3391a
Vincenzo De Pascale <119123124+VincenzoDePascale@users.noreply.github.com>
2023-10-23 04:20:04 +0200

    fix footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
+import moment from "moment";
 import Menu from "./Components/Menu";
-import Footer from "./Components/Footer";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -10,12 +10,15 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  const currentYear = moment().format("YYYY");
   return (
     <html lang="en">
       <body className={inter.className}>
         <Menu />
         {children}
-        <Footer />
+        <div className="footer-container text-xs md:fixed md:bottom-0 md:right-0">
+          <div>&copy; {currentYear} Vincenzo De Pascale</div>
+        </div>
       </body>
     </html>
   );
